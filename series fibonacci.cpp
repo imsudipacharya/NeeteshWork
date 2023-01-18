921726c0e0b1bcae3b262126abedca8cc54275f4
neeteshadhikary <76239964+neeteshadhikary@users.noreply.github.com>
2023-01-18 11:12:10 +0545

    Update series fibonacci.cpp

--- series fibonacci.cpp
+++ series fibonacci.cpp
@@ -1,26 +1,26 @@
 #include <stdio.h>
-//
-//int main() {
-//    int n, first = 0, second = 1, next, c;
-//
-//    printf("Enter the number of terms: ");
-//    scanf("%d", &n);
-//
-//    printf("Fibonacci series: ");
-//
-//    for (c = 0; c < n; c++) {
-//        if (c <= 1)
-//            next = c;
-//        else {
-//            next = first + second;
-//            first = second;
-//            second = next;
-//        }
-//        printf("%d ", next);
-//    }
-//
-//    return 0;
-//}
+
+int main() {
+    int n, first = 0, second = 1, next, c;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci series: ");
+
+    for (c = 0; c < n; c++) {
+        if (c <= 1)
+            next = c;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+
+    return 0;
+}
 
 int fibo(int n){
 	if (n==0 :: n==1)
